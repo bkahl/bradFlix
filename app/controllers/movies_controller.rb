e20f81b21fa2b9fedf6f65f348dee2d618217ee2
Brad Kahl <bkahl1984@gmail.com>
2013-11-07 21:36:07 -0500

    refactor/cleaned up code.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,86 +4,24 @@ class MoviesController < ApplicationController
   before_filter :get_all_movies
 
   def index
-    @movies = get_all_movies_by_year
-    @movies_recent = get_all_movies
-    @movies_recent = @movies_recent.sort_by { |k, v| v[:date] }
+    @all_movie_years = get_all_movies_by_year
+    @movies_by_year = get_all_movies_by_year
+    @movies_recent = get_all_movies.sort_by { |k, v| v[:date] }
   end
 
   def year
-    @movies = get_all_movies_by_year[params[:year]]
+    @all_movie_years = get_all_movies_by_year
+    @movies_by_selected_year = get_all_movies_by_year[params[:year]]
+  end
+
+  def recent
+    @all_movie_years = get_all_movies_by_year
+    @movies_recent = get_all_movies.sort_by { |k, v| v[:date] }
   end
 
   def selected
   end
 
   private
-  def get_all_movies_by_year
-    movie_path = '/Volumes/Mac.Movies/Ripped DVDs/Movies/'
-    remove_path = '/Volumes/Mac.Movies'
 
-    movie_dirs = Dir.glob(movie_path+'*')
-    movies = Hash.new{ |h,k| h[k]=Hash.new(&h.default_proc) }
-
-    movie_dirs.each do |dir|
-      curr_year = dir.split(movie_path)[1]
-      curr_year_movies = Dir.glob(dir+'/*')
-
-      curr_year_movies.each do |curr_movie_path|
-
-        curr_movie = curr_movie_path.split(movie_path+curr_year+'/')[1]
-
-        curr_logo = Dir.glob(curr_movie_path+"/*.jpg")
-        curr_logo = curr_logo[0]
-
-        date_modified = File.stat(curr_logo).mtime
-        curr_logo = curr_logo.split(remove_path)[1]
-
-        curr_mp4 = Dir.glob(curr_movie_path+"/*.m4v")
-        curr_mp4 = curr_mp4[0]
-
-        unless curr_mp4.nil?
-          curr_mp4 = curr_mp4.split(remove_path)[1]
-        end
-
-        movies[curr_year][curr_movie] = {path: curr_movie_path.split(remove_path)[1], name: curr_movie, movie: curr_mp4, logo: curr_logo, date: date_modified}
-      end
-    end
-
-    return movies
-  end
-
-  def get_all_movies
-    movie_path = '/Volumes/Mac.Movies/Ripped DVDs/Movies/'
-    remove_path = '/Volumes/Mac.Movies'
-
-    movie_dirs = Dir.glob(movie_path+'*')
-    movies = Hash.new{ |h,k| h[k]=Hash.new(&h.default_proc) }
-
-    movie_dirs.each do |dir|
-      curr_year = dir.split(movie_path)[1]
-      curr_year_movies = Dir.glob(dir+'/*')
-
-      curr_year_movies.each do |curr_movie_path|
-
-        curr_movie = curr_movie_path.split(movie_path+curr_year+'/')[1]
-
-        curr_logo = Dir.glob(curr_movie_path+"/*.jpg")
-        curr_logo = curr_logo[0]
-
-        date_modified = File.stat(curr_logo).mtime
-        curr_logo = curr_logo.split(remove_path)[1]
-
-        curr_mp4 = Dir.glob(curr_movie_path+"/*.m4v")
-        curr_mp4 = curr_mp4[0]
-
-        unless curr_mp4.nil?
-          curr_mp4 = curr_mp4.split(remove_path)[1]
-        end
-
-        movies[curr_movie] = {path: curr_movie_path.split(remove_path)[1], name: curr_movie, movie: curr_mp4, logo: curr_logo, year: curr_year, date: date_modified.to_s}
-      end
-    end
-
-    return movies
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,11 @@ BradFlix::Application.routes.draw do
 
   devise_for :users
 
-  root :to => 'recent#index'
-
-  #match recent
-  match '/recent', :to => 'recent#index'
+  root :to => 'movies#recent'
 
   #match movie
   match '/movies', :to => 'movies#index'
+  match '/movies/recent', :to => 'movies#recent'
   match '/movies/:year', :to => 'movies#year', :as => 'movie_year'
   match '/movies/:year/:movie_name', :to => 'movies#description'
 
