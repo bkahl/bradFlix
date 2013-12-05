9a8db59b3c2b352d3311feb9899d56cb40095d4d
Brad Kahl <bkahl1984@gmail.com>
2013-12-05 00:59:29 -0500

    added movie counter

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,17 +1,20 @@
 class MoviesController < ApplicationController
 
   def index
+    @count_all_movies = get_all_movies.size
     @all_movie_years = get_all_movies_by_year.reverse_each
     #@movies_by_year = get_all_movies_by_year // use for _movies partial
     @movies_alphabetically = get_all_movies.sort_by { |k, v| v[:name] }
   end
 
   def year
+    @count_all_movies = get_all_movies_by_year[params[:year]].size
     @all_movie_years = get_all_movies_by_year.reverse_each
     @movies_by_selected_year = get_all_movies_by_year[params[:year]].sort_by { |k, v| v[:date] }.reverse_each
   end
 
   def recent
+    @count_all_movies = get_all_movies.size
     @all_movie_years = get_all_movies_by_year.reverse_each
     @movies_recent = get_all_movies.sort_by { |k, v| v[:date] }
   end
