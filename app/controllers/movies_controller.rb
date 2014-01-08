5e8177fd2bf7610d54e176d94edd8647d6ea64d9
Brad Kahl <bkahl1984@gmail.com>
2014-01-08 02:28:41 -0500

    cleanup css for responsive, fix top-nav toggle-btn, added search partial, added active class to selected route to sidebar.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,14 +7,12 @@ class MoviesController < ApplicationController
     @movies_alphabetically = get_all_movies.sort_by { |k, v| v[:name] }
     @movies_recent = get_all_movies.sort_by { |k, v| v[:date] }
     @movie_names = get_all_movie_titles(@movies_recent)
-    puts @movie_names
   end
 
   def year
     @count_all_movies = get_all_movies_by_year[params[:year]].size
     @all_movie_years = get_all_movies_by_year.reverse_each
     @movies_by_selected_year = get_all_movies_by_year[params[:year]].sort_by { |k, v| v[:name] }
-
     @movies_recent = get_all_movies.sort_by { |k, v| v[:date] }
     @movie_names = get_all_movie_titles(@movies_recent)
   end
@@ -43,7 +41,6 @@ class MoviesController < ApplicationController
       movie_names.push(movie[1])
     end
 
-    puts movie_names
     return movie_names.to_json
   end
 
